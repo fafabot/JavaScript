54bd0515a22cae25a56846f3e0e0e98143378034
Rafael de Aquino <rafaeldeaquinoy@gmail.com>
2026-03-20 10:56:01 -0300

    dom2

---
+++ 18 - DOM Math/script.js
@@ -0,0 +1,25 @@
+let numero = 5.5
+let numero2 = 5.9
+
+console.log(Math.round(numero))
+console.log(Math.round(numero2))
+
+console.log(Math.floor(numero))
+console.log(Math.floor(numero2))
+
+console.log(Math.ceil(numero))
+console.log(Math.ceil(numero2))
+let n = Math.ceil(numero)
+
+console.log(Math.max(10, 5, 33, 12, 6)) //33
+console.log(Math.min(10, 5, 33, 12, 6)) //5
+
+console.log(Math.random())
+let a = Math.round(Math.random() * 10)
+console.log(a)
+
+console.log(Math.pow(2,3)) //2³
+console.log(Math.pow(57, 9))
+
+console.log(Math.sqrt(81))
+console.log(Math.sqrt(25))
