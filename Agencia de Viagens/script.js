1d9b534734d531b373a0bc1c218cc6ec776c1583
Rafael de Aquino <rafaeldeaquinoy@gmail.com>
2026-06-12 01:12:11 -0300

    ultimoooooo

--- Agencia de Viagens/script.js
+++ Agencia de Viagens/script.js
@@ -1,42 +1,41 @@
-const perguntas = document.querySelectorAll(".pergunta");
-perguntas.forEach(pergunta => {
-    pergunta.addEventListener("click", () => {
-        const resposta = pergunta.nextElementSibling;
-        resposta.style.display =
-            resposta.style.display === "block"
-            ? "none"
-            : "block";
+let perguntas = document.querySelectorAll(".pergunta");
+
+perguntas.forEach(function(pergunta) {
+    pergunta.addEventListener("click", function() {
+        let resposta = this.nextElementSibling;
+        if (resposta.style.display === "block") {
+            resposta.style.display = "none";
+        } else {
+            resposta.style.display = "block";
+        }
     });
 });
 
 
-const cards = document.querySelector(".cards");
-const btnProximo =
-document.querySelector(".proximo");
-const btnAnterior =
-document.querySelector(".anterior");
-let posicao = 0;
-btnProximo.addEventListener("click", () => {
-    posicao += 300;
-    if(posicao > cards.scrollWidth - cards.clientWidth){
-        posicao = 0;
-    }
+let cards = document.querySelector(".cards");
+let btnProximo = document.querySelector(".proximo");
+let btnAnterior = document.querySelector(".anterior");
+let paginaAtual = 0;
+
+function mostrarPagina() {
+    let larguraPagina = cards.clientWidth;
     cards.scrollTo({
-        left: posicao,
+        left: paginaAtual * larguraPagina,
         behavior: "smooth"
     });
+}
+
+btnProximo.addEventListener("click", function() {
+    paginaAtual++;
+    if (paginaAtual > 1) {
+        paginaAtual = 0;
+    }
+    mostrarPagina();
 });
-
-btnAnterior.addEventListener("click", () => {
-    posicao -= 300;
-    if(posicao < 0){
-        posicao =
-        cards.scrollWidth -
-        cards.clientWidth;
+btnAnterior.addEventListener("click", function() {
+    paginaAtual--;
+    if (paginaAtual < 0) {
+        paginaAtual = 1;
     }
-    cards.scrollTo({
-        left: posicao,
-        behavior: "smooth"
-    });
-
+    mostrarPagina();
 });
